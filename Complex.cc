ffdc294dc4856d946c861115d06305778f289c34
Masi-1796721 <92438093+Masi-1796721@users.noreply.github.com>
2021-10-18 09:26:57 +0200

    Update Complex.cc

--- Complex.cc
+++ Complex.cc
@@ -1,7 +1,18 @@
 #include <Complex.h>
 #include <iostream>
 
-Complex::Complex(){
- re=0;
- im=0;
+//constructors definition
+Complex::Complex(){ //default
+ re_=0;
+ im_=0;
+}
+
+Complex::Complex(double re, double im){ //regular
+ re_=re;
+ im_=im;
+}
+
+Complex::Complex(const Complex& complex){ //copy
+ re_=complex.re_;
+ im_=complex.im_;
 }
