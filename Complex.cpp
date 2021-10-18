f61c154faf9fef952f51611d26d18ea9a473f830
Masi-1796721 <92438093+Masi-1796721@users.noreply.github.com>
2021-10-18 10:30:09 +0200

    Update Complex.cpp

--- Complex.cpp
+++ Complex.cpp
@@ -3,7 +3,10 @@
 int main(){ //main
   
   Complex c1(3.5, 2.1); //numeri complessi
+  c1.print(); //stampo numero
+  c1.mag(); //chiamo modulo
+  c1.phase(); //chiamo fase
   
-  
+  return 0;
   
 }
