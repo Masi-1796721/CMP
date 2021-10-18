1572e1bb06bcbc35348e2becf37d130069b2302f
Masi-1796721 <92438093+Masi-1796721@users.noreply.github.com>
2021-10-18 10:02:41 +0200

    Update Complex.cc

--- Complex.cc
+++ Complex.cc
@@ -6,17 +6,17 @@
 Complex::Complex(){ //default
  re_=0;
  im_=0;
-}
+};
 
 Complex::Complex(double re, double im){ //regular
  re_=re;
  im_=im;
-}
+};
 
 Complex::Complex(const Complex& complex){ //copy
  re_=complex.re_;
  im_=complex.im_;
-}
+};
 
 Complex::~Complex(){
  
