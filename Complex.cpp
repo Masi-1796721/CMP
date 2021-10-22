554502a971d49ba83f01439f2c51e2a5f03b892a
Masi-1796721 <masi.1796721@studenti.uniroma1.it>
2021-10-22 09:41:43 +0200

    ecco le modifiche

--- Complex.cpp
+++ Complex.cpp
@@ -1,11 +1,44 @@
 #include "Complex.h"
+#include <iostream>
+
+using namespace std;
 
 int main(){ //main
   
-  Complex c1(3.5, 2.1); //numeri complessi
-  c1.print(); //stampo numero
-  c1.mag(); //chiamo modulo
-  c1.phase(); //chiamo fase
+  Complex c1(3.5, 2.1), c2(-1.1, 2); //numeri complessi
+  
+  //print data
+  c1.print();
+  c1.mag();
+  c1.phase();
+  c2.print();
+  c2.mag();
+  c2.phase();
+  
+  //overloaded operations
+  Complex c3 = c1 + c2;
+  cout<<"sum result: ";
+  c3.print();
+  
+  Complex c4 = c1 - c2;
+  cout<<"subtraction result: ";
+  c4.print();
+  
+  Complex c5 = c1/c2;
+  cout<<"division result: ";
+  c5.print();
+  
+  Complex c6 = c1*c2;
+  cout<<"multiplication result: ";
+  c6.print();
+  
+  Complex c7 = c1*0.5;
+  cout<<"multiplication result: ";
+  c7.print();
+  
+  Complex c8 = 0.5*c1;
+  cout<<"multiplication result: ";
+  c8.print();
   
   return 0;
   
